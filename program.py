5a005ba4e2e847e6e94fde9dbe7c13fef113ea29
Nikolay Milenko <NikolayL2sch@users.noreply.github.com>
2021-02-25 23:50:36 +0300

    created class record

--- program.py
+++ program.py
@@ -1,15 +1,25 @@
 # requires pytest, pip, pytest-html
 
 # here should be the imports
+import datetime as dt
 
 # parent-class
 class Calc:
     def __init__(self, limit):
         self.limit = limit
-        records = []
-    def add_record (self,other)
+        self.records = []
+    def add_record (self,Record):
+        records.append(Record)
+
 class Record:
-    def __init__(self, amount, comment, date):
+    def __init__(self, amount, comment, date = None):
+        date_format = '%d/%m/%y'
         self.amount = amount
         self.comment = comment
-        self.date = date
+        if date is None:
+            self.date = dt.date.today()
+        else:
+            self.date = dt.datetime.strptime(date, date_format).date()
+
+class CalorieCalc(Calc):
+    
