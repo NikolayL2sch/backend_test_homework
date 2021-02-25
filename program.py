1c983fbb73d41096eac9529222c0c2f94cc5f273
Nikolay Milenko <NikolayL2sch@users.noreply.github.com>
2021-02-25 23:26:37 +0300

    added parent class

--- program.py
+++ program.py
@@ -1,11 +1,15 @@
-# content of test_sysexit.py
-import pytest
+# requires pytest, pip, pytest-html
 
+# here should be the imports
 
-def f():
-    raise SystemExit(1)
-
-
-def test_mytest():
-    with pytest.raises(SystemExit):
-        f()
+# parent-class
+class Calc:
+    def __init__(self, limit):
+        self.limit = limit
+        records = []
+    def add_record (self,other)
+class Record:
+    def __init__(self, amount, comment, date):
+        self.amount = amount
+        self.comment = comment
+        self.date = date
